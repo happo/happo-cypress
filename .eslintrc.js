b980880aa33c4913403248d6e934c6c6e9bcd7bd
Henric Trotzig <henric.trotzig@gmail.com>
2020-05-06 07:41:19 +0200

    Add eslint setup
    I'm about to fix an issue that would have been easily found using a tool
    like eslint.
    
    This commit was prepared by running `eslint --init` and adding some
    things manually to .eslintrc.js:
    - A bunch of globals
    - Ignoring pages directory (this folder contains React code and I'm not
      going to bother setting up linting for those since they aren't
      important in the library anyway)

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    node: true,
+  },
+  extends: 'eslint:recommended',
+  globals: {
+    Atomics: 'readonly',
+    Cypress: 'readonly',
+    SharedArrayBuffer: 'readonly',
+    after: 'readonly',
+    before: 'readonly',
+    cy: 'readonly',
+    describe: 'readonly',
+    it: 'readonly',
+  },
+  ignorePatterns: ['pages/'],
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 2018,
+  },
+  rules: {},
+};
