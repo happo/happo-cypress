905e67a8b48a44402c786536e0e2355f0353f674
Henric Trotzig <henric.trotzig@gmail.com>
2020-08-28 09:55:05 +0200

    WIP: introduce assertion failure
    I want to know how things behave when a spec fails.

--- cypress/integration/second_page_spec.js
+++ cypress/integration/second_page_spec.js
@@ -1,6 +1,7 @@
 describe('A different page', () => {
   it('has happo tests', () => {
     cy.visit('/');
+    cy.get('body').should('have.class', 'nope');
     cy.get('.card').happoScreenshot({ component: 'Card', variant: 'from page' });
   });
 });
