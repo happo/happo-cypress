happo/happo-cypress

commit bcca52600c062a9275b928cee46d0bce0bba7da1

Fix making external asset urls absolute
When we discover an external CSS file, we download it and send it down
as `globalCSS`. An important step before we can inject into `globalCSS`
is that we "externalize" all urls found in the CSS file. E.g.

  url(/foo/bar.png);

needs to become

  url(https://base.url/foo/bar.png);

otherwise the asset won't be found on the happo workers.

src/makeExternalUrlsAbsolute.js
@@ -1,13 +1,12 @@
-const { URL } = require('url');
+const replaceAll = require('string.prototype.replaceall');
 
 const findCSSAssetUrls = require('./findCSSAssetUrls');
 
-module.exports = function makeExternalUrlsAbsolute(text, absUrl) {
-  const assetUrls = findCSSAssetUrls(text);
-  let result = text;
-  for (const url of assetUrls) {
-    const fullUrl = new URL(url, absUrl);
-    result = result.split(url).join(fullUrl.href);
-  }
-  return result;
+module.exports = function makeExternalUrlsAbsolute(text, baseUrl) {
+  return replaceAll(text, findCSSAssetUrls.URL_PATTERN, (full, pre, url, post) => {
+    if (url.startsWith('data:')) {
+      return full;
+    }
+    return `${pre}${baseUrl}${url}${post}`;
+  });
 };

test/makeExternalUrlsAbsolute-test.js
@@ -1,0 +1,45 @@
+const assert = require('assert');
+
+const makeExternalUrlsAbsolute = require('../src/makeExternalUrlsAbsolute');
+
+const baseUrl = 'https://base.url';
+
+function runTest() {
+  // updates urls
+  assert.equal(
+    makeExternalUrlsAbsolute(
+      `
+    .foo {
+      background-image: url("/bar.png");
+    }
+    @font-face {
+      font-family: "MyFont";
+      src: url('/fonts/myfont.woff2') format("woff2"),
+             url(/fonts/myfont.woff) format("woff");
+    }
+    .ignore {
+      background: url(data:image/png;base64,asdf);
+    }
+  `.trim(),
+      baseUrl,
+    ),
+    `
+    .foo {
+      background-image: url("https://base.url/bar.png");
+    }
+    @font-face {
+      font-family: "MyFont";
+      src: url('https://base.url/fonts/myfont.woff2') format("woff2"),
+             url(https://base.url/fonts/myfont.woff) format("woff");
+    }
+    .ignore {
+      background: url(data:image/png;base64,asdf);
+    }
+  `.trim(),
+  );
+  // can deal with empty input
+  assert.equal(makeExternalUrlsAbsolute('', baseUrl), '');
+}
+
+runTest();
+console.log('All makeExternalUrlsAbsolute tests passed');

src/findCSSAssetUrls.js
@@ -1,11 +1,11 @@
-const URL_PATTERN = /(?:url\(['"]?)(.*?)(?:['"]?)\)/g;
+const URL_PATTERN = /(url\(['"]?)(.*?)(['"]?\))/g;
 
-module.exports = function findCSSAssetUrls(string) {
+function findCSSAssetUrls(string) {
   const result = [];
   let match;
   // eslint-disable-next-line no-cond-assign
   while ((match = URL_PATTERN.exec(string))) {
-    const url = match[1];
+    const url = match[2];
     if (!url.startsWith('data:')) {
       result.push(url);
     }
@@ -13,3 +13,5 @@ module.exports = function findCSSAssetUrls(string) {
   return result;
 }
 
+findCSSAssetUrls.URL_PATTERN = URL_PATTERN;
+module.exports = findCSSAssetUrls;

test/findCSSAssetUrls-test.js
@@ -1,0 +1,27 @@
+const assert = require('assert');
+
+const findCSSAssetUrls = require('../src/findCSSAssetUrls');
+
+function runTest() {
+  assert.deepEqual(
+    findCSSAssetUrls(
+      `
+        .foo {
+          background-image: url("/bar.png");
+        }
+        @font-face {
+          font-family: "MyFont";
+          src: url('/fonts/myfont.woff2') format("woff2"),
+                 url(/fonts/myfont.woff) format("woff");
+        }
+        .ignore {
+          background: url(data:image/png;base64,asdfasdfasfd);
+        }
+      `.trim(),
+    ),
+    ['/bar.png', '/fonts/myfont.woff2', '/fonts/myfont.woff'],
+  );
+}
+
+runTest();
+console.log('All findCSSAssetUrls tests passed');

commit 97fbc66d38e9eba6f850c2c2c10554699e7e15dc

Fix makeExternalUrlsAbsolute handling of relative urls
I introduced a regression in bcca526 where relative urls would no longer
be correctly resolved. Bringing back some old url handling fixes the
issue.

src/makeExternalUrlsAbsolute.js
@@ -1,12 +1,18 @@
+const { URL } = require('url');
 const replaceAll = require('string.prototype.replaceall');
 
 const findCSSAssetUrls = require('./findCSSAssetUrls');
 
-module.exports = function makeExternalUrlsAbsolute(text, baseUrl) {
-  return replaceAll(text, findCSSAssetUrls.URL_PATTERN, (full, pre, url, post) => {
-    if (url.startsWith('data:')) {
-      return full;
-    }
-    return `${pre}${baseUrl}${url}${post}`;
-  });
+module.exports = function makeExternalUrlsAbsolute(text, absUrl) {
+  return replaceAll(
+    text,
+    findCSSAssetUrls.URL_PATTERN,
+    (full, pre, url, post) => {
+      if (url.startsWith('data:')) {
+        return full;
+      }
+      const fullUrl = new URL(url, absUrl);
+      return `${pre}${fullUrl.href}${post}`;
+    },
+  );
 };

test/makeExternalUrlsAbsolute-test.js
@@ -2,7 +2,7 @@ const assert = require('assert');
 
 const makeExternalUrlsAbsolute = require('../src/makeExternalUrlsAbsolute');
 
-const baseUrl = 'https://base.url';
+const resourceUrl = 'https://base.url/styles/styles.min.css';
 
 function runTest() {
   // updates urls
@@ -20,8 +20,12 @@ function runTest() {
     .ignore {
       background: url(data:image/png;base64,asdf);
     }
+    .relative {
+      background: url(../one.png);
+      background-image: url(two.png);
+    }
   `.trim(),
-      baseUrl,
+      resourceUrl,
     ),
     `
     .foo {
@@ -35,10 +39,14 @@ function runTest() {
     .ignore {
       background: url(data:image/png;base64,asdf);
     }
+    .relative {
+      background: url(https://base.url/one.png);
+      background-image: url(https://base.url/styles/two.png);
+    }
   `.trim(),
   );
   // can deal with empty input
-  assert.equal(makeExternalUrlsAbsolute('', baseUrl), '');
+  assert.equal(makeExternalUrlsAbsolute('', resourceUrl), '');
 }
 
 runTest();