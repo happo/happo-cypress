3312b183b2d810f185c2e030e9747f78c7b97122
Henric Trotzig <henric.trotzig@gmail.com>
2021-11-16 17:27:54 +0100

    Extract code into happo-e2e library
    This new library will be the common driver for all end-to-end Happo
    integrations, like Cypress, Playwright and others.
    
    The happo-e2e repository is located at
    https://github.com/happo/happo-e2e, here are the changes where files
    have moved over:
    
    https://github.com/happo/happo-e2e/commit/8784c4d79afc437eccbb05bb31a4848937bc3652
    https://github.com/happo/happo-e2e/commit/bb7c763faddd147b55c3d739215d6e70c424d3f2
    https://github.com/happo/happo-e2e/commit/60638c78d563cdf67fe45b20e5fd10a70c6e2a00
    
    The interface between happo-cypress and happo-e2e has been simplified so
    that other e2e environments can use it as well.

--- task.js
+++ task.js
@@ -1,260 +1,23 @@
-const crypto = require('crypto');
-const fs = require('fs');
+const Controller = require('happo-e2e/controller');
 
-const mkdirp = require('mkdirp');
-const nodeFetch = require('node-fetch');
+const controller = new Controller();
 
-const makeRequest = require('happo.io/build/makeRequest').default;
-const { RemoteBrowserTarget } = require('happo.io');
+const localSnapshotImages = {};
 
-const createAssetPackage = require('./src/createAssetPackage');
-const convertBase64FileToReal = require('./src/convertBase64FileToReal');
-const proxiedFetch = require('./src/fetch');
-const findCSSAssetUrls = require('./src/findCSSAssetUrls');
-const loadHappoConfig = require('./src/loadHappoConfig');
-const makeAbsolute = require('./src/makeAbsolute');
-const makeExternalUrlsAbsolute = require('./src/makeExternalUrlsAbsolute');
-const resolveEnvironment = require('./src/resolveEnvironment');
-
-const { HAPPO_CYPRESS_PORT, HAPPO_DEBUG, HAPPO_ENABLED } = process.env;
-
-let snapshots;
-let localSnapshots;
-let localSnapshotImages;
-let allCssBlocks;
-let snapshotAssetUrls;
-let happoConfig;
-let knownComponentVariants = {};
-
-function getUniqueUrls(urls) {
-  const seenKeys = new Set();
-  const result = [];
-  urls.forEach(url => {
-    const key = [url.url, url.baseUrl].join('||');
-    if (!seenKeys.has(key)) {
-      result.push(url);
-      seenKeys.add(key);
-    }
-  });
-  return urls;
-}
-
-async function downloadCSSContent(blocks) {
-  const promises = blocks.map(async block => {
-    if (block.href) {
-      const absUrl = makeAbsolute(block.href, block.baseUrl);
-      if (HAPPO_DEBUG) {
-        console.log(`[HAPPO] Downloading CSS file from ${absUrl}`);
-      }
-      const res = await proxiedFetch(absUrl);
-      if (!res.ok) {
-        console.warn(
-          `[HAPPO] Failed to fetch CSS file from ${block.href}. This might mean styles are missing in your Happo screenshots`,
-        );
-        return;
-      }
-      let text = await res.text();
-      if (HAPPO_DEBUG) {
-        console.log(
-          `[HAPPO] Done downloading CSS file from ${absUrl}. Got ${text.length} chars back.`,
-        );
-      }
-      if (!absUrl.startsWith(block.baseUrl)) {
-        text = makeExternalUrlsAbsolute(text, absUrl);
-      }
-      block.content = text;
-      block.assetsBaseUrl = absUrl.replace(/\/[^/]*$/, '/');
-      delete block.href;
-    }
-  });
-  await Promise.all(promises);
-}
-
-function dedupeVariant(component, variant) {
-  knownComponentVariants[component] = knownComponentVariants[component] || {};
-  const comp = knownComponentVariants[component];
-  comp[variant] = comp[variant] || 0;
-  comp[variant]++;
-  if (comp[variant] === 1) {
-    return variant;
-  }
-  return `${variant}-${comp[variant]}`;
-}
-
-function dedupeSnapshots() {
-  for (const snapshot of snapshots) {
-    snapshot.variant = dedupeVariant(snapshot.component, snapshot.variant);
-  }
-}
-
-function handleDynamicTargets(targets) {
-  const result = [];
-  if (typeof targets === 'undefined') {
-    // return non-dynamic targets from .happo.js
-    return Object.keys(happoConfig.targets).filter(
-      targetName => !happoConfig.targets[targetName].__dynamic,
-    );
-  }
-  for (const target of targets) {
-    if (typeof target === 'string') {
-      result.push(target);
-    }
-    if (
-      typeof target === 'object' &&
-      target.name &&
-      target.viewport &&
-      target.browser
-    ) {
-      if (happoConfig.targets[target.name]) {
-        // already added
-      } else {
-        // add dynamic target
-        happoConfig.targets[target.name] = new RemoteBrowserTarget(
-          target.browser,
-          target,
-        );
-        happoConfig.targets[target.name].__dynamic = true;
-      }
-      result.push(target.name);
-    }
-  }
-  return result;
-}
-
-async function uploadLocalSnapshots() {
-  const reportResult = await makeRequest(
-    {
-      url: `${happoConfig.endpoint}/api/snap-requests/with-results`,
-      method: 'POST',
-      json: true,
-      body: {
-        snaps: localSnapshots,
-      },
-    },
-    { ...happoConfig, maxTries: 3 },
-  );
-  return reportResult.requestId;
-}
-
-async function uploadImage(pathToFile) {
-  if (HAPPO_DEBUG) {
-    console.log(`[HAPPO] Uploading image: ${pathToFile}`);
-  }
-  const buffer = await fs.promises.readFile(pathToFile);
-  const hash = crypto.createHash('md5').update(buffer).digest('hex');
-
-  const uploadUrlResult = await makeRequest(
-    {
-      url: `${happoConfig.endpoint}/api/images/${hash}/upload-url`,
-      method: 'GET',
-      json: true,
-    },
-    { ...happoConfig, maxTries: 2 },
-  );
-
-  if (!uploadUrlResult.uploadUrl) {
-    // image has already been uploaded
-    if (HAPPO_DEBUG) {
-      console.log(
-        `[HAPPO] Image has already been uploaded: ${uploadUrlResult.url}`,
-      );
-    }
-    return uploadUrlResult.url;
-  }
-
-  const uploadResult = await makeRequest(
-    {
-      url: uploadUrlResult.uploadUrl,
-      method: 'POST',
-      json: true,
-      formData: {
-        file: {
-          options: {
-            filename: 'image.png',
-            contentType: 'image/png',
-          },
-          value: buffer,
-        },
-      },
-    },
-    { ...happoConfig, maxTries: 2 },
-  );
-  if (HAPPO_DEBUG) {
-    console.log(`[HAPPO] Uploaded image: ${uploadUrlResult.url}`);
-  }
-  return uploadResult.url;
-}
-
-async function processSnapRequestIds(allRequestIds) {
-  if (HAPPO_CYPRESS_PORT) {
-    // We're running with `happo-cypress --`
-    const fetchRes = await nodeFetch(
-      `http://localhost:${HAPPO_CYPRESS_PORT}/`,
-      {
-        method: 'POST',
-        body: allRequestIds.join('\n'),
-      },
-    );
-    if (!fetchRes.ok) {
-      throw new Error('Failed to communicate with happo-cypress server');
-    }
-  } else {
-    // We're not running with `happo-cypress --`. We'll create a report
-    // despite the fact that it might not contain all the snapshots. This is
-    // still helpful when running `cypress open` locally.
-    const { afterSha } = resolveEnvironment();
-    const reportResult = await makeRequest(
-      {
-        url: `${happoConfig.endpoint}/api/async-reports/${afterSha}`,
-        method: 'POST',
-        json: true,
-        body: {
-          requestIds: allRequestIds,
-          project: happoConfig.project,
-        },
-      },
-      { ...happoConfig, maxTries: 3 },
-    );
-    console.log(`[HAPPO] ${reportResult.url}`);
-
-    // Reset the component variants so that we can run the test again while
-    // cypress is still open.
-    knownComponentVariants = {};
-    return null;
-  }
-}
-
-function removeSnapshotsMadeBetween({ start, end }) {
-  if (HAPPO_DEBUG) {
-    console.log(
-      `[HAPPO] Removing snapshots made between ${new Date(
-        start,
-      )} and ${new Date(end)}`,
-    );
-  }
-  snapshots = snapshots.filter(({ timestamp }) => {
-    if (!timestamp) {
-      return true;
-    }
-    return timestamp < start || timestamp > end;
-  });
-}
+const { HAPPO_DEBUG } = process.env;
 
 const task = {
   register(on) {
     on('task', task);
-    on('after:screenshot', task.handleAfterScreenshot);
+    on('before:spec', task.handleBeforeSpec);
     on('after:spec', task.handleAfterSpec);
-    on('before:spec', task.happoInit);
+    on('after:screenshot', task.handleAfterScreenshot);
     task.isRegisteredCorrectly = true;
   },
 
   async handleAfterSpec(spec, results) {
-    if (!happoConfig) {
+    if (!controller.isActive()) {
       return;
-    }
-    if (HAPPO_DEBUG) {
-      console.log('[HAPPO] Running after:spec hook');
     }
     for (const test of results.tests) {
       const wasRetried =
@@ -266,7 +29,7 @@ const task = {
       for (const attempt of test.attempts) {
         if (attempt.state === 'failed') {
           const start = new Date(attempt.wallClockStartedAt).getTime();
-          removeSnapshotsMadeBetween({
+          controller.removeSnapshotsMadeBetween({
             start,
             end: start + attempt.wallClockDuration,
           });
@@ -274,63 +37,34 @@ const task = {
       }
     }
 
-    await task.happoTeardown();
+    await controller.finish();
     return null;
   },
 
-  happoRegisterSnapshot({
-    timestamp,
-    html,
-    assetUrls,
-    cssBlocks,
-    component,
-    variant,
-    targets: rawTargets,
-    htmlElementAttrs,
-    bodyElementAttrs,
-  }) {
-    if (!happoConfig) {
+  async happoRegisterSnapshot(snapshot) {
+    if (!controller.isActive()) {
       return null;
     }
-    snapshotAssetUrls.push(...assetUrls);
-    const targets = handleDynamicTargets(rawTargets);
-    snapshots.push({
-      timestamp,
-      html,
-      component,
-      variant,
-      targets,
-      htmlElementAttrs,
-      bodyElementAttrs,
-    });
-    cssBlocks.forEach(block => {
-      if (allCssBlocks.some(b => b.key === block.key)) {
-        return;
-      }
-      allCssBlocks.push(block);
-    });
+    await controller.registerSnapshot(snapshot);
     return null;
   },
 
   happoRegisterLocalSnapshot({
     imageId,
     component,
-    variant: rawVariant,
-    targets,
+    variant,
     target,
+    targets,
   }) {
-    if (!happoConfig) {
-      return null;
-    }
-    const variant = dedupeVariant(component, rawVariant);
     localSnapshotImages[imageId] = { component, variant, targets, target };
     return null;
   },
 
   async handleAfterScreenshot({ name, path, dimensions }) {
-    if (!happoConfig) {
+    if (!controller.isActive()) {
       return null;
     }
+
     const snapshotData = localSnapshotImages[name];
     if (!snapshotData) {
       if (HAPPO_DEBUG) {
@@ -339,74 +73,31 @@ const task = {
       return;
     }
 
-    const { component, variant, target } = snapshotData;
-    localSnapshots.push({
-      component,
-      variant,
-      target,
-      url: await uploadImage(path),
+    await controller.registerLocalSnapshot({
+      ...snapshotData,
       ...dimensions,
+      path,
     });
     return null;
   },
 
   async happoRegisterBase64Image({ base64Chunk, src, isFirst, isLast }) {
-    const filename = src.slice(1);
-    const filenameB64 = `${filename}.b64`;
-    if (isFirst) {
-      await mkdirp('.happo-tmp/_inlined');
-      await new Promise((resolve, reject) =>
-        fs.writeFile(filenameB64, base64Chunk, e => {
-          if (e) {
-            reject(e);
-          } else {
-            resolve();
-          }
-        }),
-      );
-    } else {
-      await new Promise((resolve, reject) =>
-        fs.appendFile(filenameB64, base64Chunk, e => {
-          if (e) {
-            reject(e);
-          } else {
-            resolve();
-          }
-        }),
-      );
+    if (!controller.isActive()) {
+      return null;
     }
-
-    if (isLast) {
-      await convertBase64FileToReal(filenameB64, filename);
-    }
+    await controller.registerBase64ImageChunk({
+      base64Chunk,
+      src,
+      isFirst,
+      isLast,
+    });
     return null;
   },
 
-  async happoInit() {
-    snapshots = [];
-    allCssBlocks = [];
-    snapshotAssetUrls = [];
-    localSnapshots = [];
-    localSnapshotImages = {};
-    if (!(HAPPO_CYPRESS_PORT || HAPPO_ENABLED)) {
-      console.log(
-        `
-[HAPPO] Happo is disabled. Here's how to enable it:
-  - Use the \`happo-cypress\` wrapper when running \`cypress run\`.
-  - Set \`HAPPO_ENABLED=true\` when running \`cypress open\`.
+  async handleBeforeSpec() {
+    await controller.init();
 
-Docs:
-  https://docs.happo.io/docs/cypress#usage-with-cypress-run
-  https://docs.happo.io/docs/cypress#usage-with-cypress-open
-      `.trim(),
-      );
-      return null;
-    }
-    if (HAPPO_DEBUG) {
-      console.log('[HAPPO] Running happoInit');
-    }
-    happoConfig = await loadHappoConfig();
-    if (happoConfig && !task.isRegisteredCorrectly) {
+    if (controller.isActive() && !task.isRegisteredCorrectly) {
       throw new Error(`happo-cypress hasn't been registered correctly. Make sure you call \`happoTask.register\` when you register the plugin:
 
   const happoTask = require('happo-cypress/task');
@@ -416,97 +107,6 @@ Docs:
   };
       `);
     }
-    return null;
-  },
-
-  async happoTeardown() {
-    if (!happoConfig) {
-      return null;
-    }
-    if (HAPPO_DEBUG) {
-      console.log('[HAPPO] Running happoTeardown');
-    }
-    if (localSnapshots.length) {
-      await processSnapRequestIds([await uploadLocalSnapshots()]);
-      return null;
-    }
-    if (!snapshots.length) {
-      return null;
-    }
-    dedupeSnapshots();
-    await downloadCSSContent(allCssBlocks);
-    const allUrls = [...snapshotAssetUrls];
-    allCssBlocks.forEach(block => {
-      findCSSAssetUrls(block.content).forEach(url =>
-        allUrls.push({ url, baseUrl: block.assetsBaseUrl || block.baseUrl }),
-      );
-    });
-
-    const uniqueUrls = getUniqueUrls(allUrls);
-    const { buffer, hash } = await createAssetPackage(uniqueUrls);
-
-    if (HAPPO_DEBUG) {
-      console.log(`[HAPPO] Uploading assets package`);
-    }
-    const assetsRes = await makeRequest(
-      {
-        url: `${happoConfig.endpoint}/api/snap-requests/assets/${hash}`,
-        method: 'POST',
-        json: true,
-        formData: {
-          payload: {
-            options: {
-              filename: 'payload.zip',
-              contentType: 'application/zip',
-            },
-            value: buffer,
-          },
-        },
-      },
-      { ...happoConfig, maxTries: 3 },
-    );
-    if (HAPPO_DEBUG) {
-      console.log('[HAPPO] Done uploading assets package, got', assetsRes);
-    }
-
-    let globalCSS = allCssBlocks.map(block => block.content).join('\n');
-    for (const url of uniqueUrls) {
-      if (/^\/_external\//.test(url.name) && url.name !== url.url) {
-        globalCSS = globalCSS.split(url.url).join(url.name);
-        snapshots.forEach(snapshot => {
-          snapshot.html = snapshot.html.split(url.url).join(url.name);
-        });
-      }
-    }
-    const allRequestIds = [];
-    for (const name of Object.keys(happoConfig.targets)) {
-      if (HAPPO_DEBUG) {
-        console.log(`[HAPPO] Sending snap-request(s) for target=${name}`);
-      }
-      const snapshotsForTarget = snapshots.filter(
-        ({ targets }) => !targets || targets.includes(name),
-      );
-      const requestIds = await happoConfig.targets[name].execute({
-        targetName: name,
-        asyncResults: true,
-        endpoint: happoConfig.endpoint,
-        globalCSS,
-        assetsPackage: assetsRes.path,
-        snapPayloads: snapshotsForTarget,
-        apiKey: happoConfig.apiKey,
-        apiSecret: happoConfig.apiSecret,
-      });
-      if (HAPPO_DEBUG) {
-        console.log(
-          `[HAPPO] Snap-request(s) for target=${name} created with ID(s)=${requestIds.join(
-            ',',
-          )}`,
-        );
-      }
-      allRequestIds.push(...requestIds);
-    }
-
-    await processSnapRequestIds(allRequestIds);
     return null;
   },
 };
