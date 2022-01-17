664f0660cbca9f0e5d60025848d166ffd38a2fe4
Henric Trotzig <henric.trotzig@gmail.com>
2022-01-17 15:40:05 +0100

    Wait for big canvas before taking screenshot
    There was a race condition here where the image might not have been
    loaded (and injected into the canvas) at the time we call
    happoScreenshot. We can wait for the class that gets added and we'll be
    better off.

--- cypress/integration/bigcanvas_spec.js
+++ cypress/integration/bigcanvas_spec.js
@@ -1,6 +1,7 @@
 describe('Big canvas', () => {
   it('has screenshots', () => {
     cy.visit('/big-canvas');
+    cy.get('.canvas-loaded');
     cy.get('body').happoScreenshot({
       component: 'Big canvas',
       variant: 'default',
