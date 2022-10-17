1e206de1d9fc98f7478b956fd2013db3f20da47c
Henric Trotzig <henric.trotzig@gmail.com>
2022-10-17 09:40:14 +0200

    Set <base> for hello page
    This will help ensure that styles are fetched from the right place.

--- pages/subfolder/hello.js
+++ pages/subfolder/hello.js
@@ -1,8 +1,12 @@
 import React, { useEffect } from 'react';
+import Head from 'next/head';
 
 export default function HelloPage() {
   return (
     <div>
+      <Head>
+        <base href="/" />
+      </Head>
       <h1>Hello world. I'm red.</h1>
     </div>
   );
